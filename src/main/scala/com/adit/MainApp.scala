08233caeeb3d3005f6c4bd837edb785230ff1e1c
aditya hadiwijaya <aditya.hadiwijaya@mncgroup.com>
2018-02-14 21:51:26 +0700

    get config then put into database service then init route

---
+++ src/main/scala/com/adit/MainApp.scala
@@ -0,0 +1,31 @@
+import akka.actor.ActorSystem
+import akka.event.Logging
+import akka.stream.ActorMaterializer
+import com.typesafe.config.ConfigFactory
+import http.ItemRoute
+import services.{DatabaseService, ItemService}
+import slick.jdbc.MySQLProfile
+import akka.http.scaladsl.Http
+
+object MainApp extends App{
+    implicit val system = ActorSystem()
+    implicit val executor = system.dispatcher
+    implicit val materializer = ActorMaterializer()
+
+    val config = ConfigFactory.load()
+    val logger = Logging(system, getClass)
+
+    val dbConfig = config.getConfig("inventorydb")
+    val DatabaseService = new DatabaseService(
+        dbConfig.getString("url"), 
+        dbConfig.getString("user"), 
+        dbConfig.getString("password"), 
+        MySQLProfile)
+
+    val itemService = new ItemService(DatabaseService)
+    val ItemRoute = new ItemRoute(itemService)
+    val route = ItemRoute.route
+
+    val port = if (sys.env.contains("PORT")) sys.env("PORT").toInt else config.getInt("http.port")
+    Http().bindAndHandle(route, "localhost", port)
+}
