bf57dd077950ef6f17877d1d888a5db4ea2d93eb
aditya hadiwijaya <aditya.hadiwijaya@mncgroup.com>
2018-02-14 22:02:30 +0700

    fix missing val on driver

--- src/main/scala/com/adit/services/DatabaseService.scala
+++ src/main/scala/com/adit/services/DatabaseService.scala
@@ -4,7 +4,7 @@ import com.zaxxer.hikari.{HikariConfig, HikariDataSource}
 import models.db.DAO
 import slick.jdbc.JdbcProfile
 
-class DatabaseService(jdbcUrl:String, user:String, password:String, driver:JdbcProfile){
+class DatabaseService(jdbcUrl:String, user:String, password:String, val driver:JdbcProfile){
     private val hikariConfig = new HikariConfig()
     hikariConfig.setJdbcUrl(jdbcUrl)
     hikariConfig.setUsername(user)
