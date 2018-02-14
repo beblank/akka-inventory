1b5a667f760bb625e1ba31560e1bda8592cbdda5
aditya hadiwijaya <aditya.hadiwijaya@mncgroup.com>
2018-02-15 05:49:30 +0700

    refactor change id into long not sku

--- src/main/scala/com/adit/services/ItemService.scala
+++ src/main/scala/com/adit/services/ItemService.scala
@@ -10,12 +10,12 @@ class ItemService(val databaseService:DatabaseService)(implicit val executionCon
     val items = TableQuery[daoType.Items]
 
     def getItems():Future[Seq[Item]] = db.run(items.result)
-    def getItem(sku:String):Future[Item] = db.run(items.filter(_.sku === sku).result.headOption)
-    def createItem(item:Item):Future[Item] = db.run(items returning items.map(_.sku) into ((item, sku) => item.copy(sku=Some(sku))) += item)
-    def updateItem(sku:String, toUpdate:Item):Future[Option[Item]] = getItem(sku).flatMap{
+    def getItem(id:Long):Future[Option[Item]] = db.run(items.filter(_.id === id).result.headOption)
+    def createItem(item:Item):Future[Item] = db.run(items returning items.map(_.id) into ((item, id) => item.copy(id=Some(id))) += item)
+    def updateItem(id:Long, toUpdate:Item):Future[Option[Item]] = getItem(id).flatMap{
         case Some(item) => {
-            val updateItem = Item(item.sku, toUpdate.name, toUpdate.qty)
-            db.run(items.filter(_.sku === sku).update(updateItem)).map(_ => Some(updateItem))
+            val updateItem = Item(item.id, toUpdate.sku, toUpdate.name, toUpdate.qty)
+            db.run(items.filter(_.id === id).update(updateItem)).map(_ => Some(updateItem))
         }
         case None => Future.successful(None)
     }
