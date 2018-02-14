543670a3b7269e8b6bf8174cca7bc59efa492e61
aditya hadiwijaya <aditya.hadiwijaya@mncgroup.com>
2018-02-14 21:42:39 +0700

    api route for item using itemservice

--- src/main/scala/com/adit/http/ItemRoute.scala
+++ src/main/scala/com/adit/http/ItemRoute.scala
@@ -1,7 +1,7 @@
 package http
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import akka.http.scaladsl.server.Directive._
+import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.model.StatusCodes._
 import models.Item
 import services.ItemService
@@ -9,4 +9,39 @@ import spray.json.DefaultJsonProtocol
 
 trait Protocols extends SprayJsonSupport with DefaultJsonProtocol{
     implicit val itemFormat = jsonFormat3(Item)
+}
+
+class ItemRoute(val itemService:ItemService) extends Protocol{
+    val route = logRequestResult("ItemRoutes"){
+        pathPrefix("items"){
+            pathEndOrSingleSlash{
+                get{
+                    complete{
+                        itemService.getItems()
+                    }
+                } ~
+                post{
+                    entity(as[Item]){ itemCreate =>
+                        complete{
+                            itemService.createItem(itemCreate)
+                        }
+                    }
+                }
+            } ~ 
+            pathPrefix(String){ sku =>
+                get {
+                    complete{
+                        itemService.getItem(sku)
+                    }
+                } ~
+                put {
+                    entity(as[Item]){ itemUpdate => 
+                        complete{
+                            itemService.updateItem(sku, itemUpdate)
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
