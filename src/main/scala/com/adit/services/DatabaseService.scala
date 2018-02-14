593a39c22c584c545133081472d3f03431f0a67a
aditya hadiwijaya <aditya.hadiwijaya@mncgroup.com>
2018-02-14 21:20:09 +0700

    migrate db using flyway then config slick

--- src/main/scala/com/adit/services/DatabaseService.scala
+++ src/main/scala/com/adit/services/DatabaseService.scala
@@ -12,5 +12,13 @@ class DatabaseService(jdbcUrl:String, user:String, password:String, driver:JdbcP
 
     val dataSource = new HikariDataSource(hikariConfig)
 
+    private val flywayService = new FlywayService(dataSource)
+    flywayService.migrateDatabaseSchema()
+
+    import driver.api._
+    val db = Database.forDataSource(dataSource, Some(10))
+
+    val daoType = DAO(driver)
+    db.createSession()
 }
 
