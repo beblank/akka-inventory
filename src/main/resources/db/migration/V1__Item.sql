36036e190aaaf8336960f48c72447c8afbc84915
aditya hadiwijaya <aditya.hadiwijaya@mncgroup.com>
2018-02-15 05:36:48 +0700

    change db to include item id

--- src/main/resources/db/migration/V1__Item.sql
+++ src/main/resources/db/migration/V1__Item.sql
@@ -1,5 +1,6 @@
-CREATE TABLE ITEM (
-  item_sku VARCHAR(100) PRIMARY KEY,
+CREATE TABLE ITEMS (
+  item_id BIGINT PRIMARY KEY,
+  item_sku VARCHAR(25),
   item_name VARCHAR(100) NOT NULL,
   item_qty INT NOT NULL
 );
