e185035858657623f978fd87c9dba22bf3b7351f
aditya hadiwijaya <aditya.hadiwijaya@mncgroup.com>
2018-02-15 11:57:05 +0700

    add item in services

--- src/main/scala/com/adit/services/ItemService.scala
+++ src/main/scala/com/adit/services/ItemService.scala
@@ -1,13 +1,16 @@
 package services
 
 import models.Item
+import models.ItemIn
 import scala.concurrent.{ExecutionContext, Future}
+import java.sql.Timestamp
 
 class ItemService(val databaseService:DatabaseService)(implicit val executionContext:ExecutionContext){
     import databaseService._
     import databaseService.driver.api._
 
     val items = TableQuery[daoType.Items]
+    val itemsIn = TableQuery[daoType.ItemsIn]
 
     def getItems():Future[Seq[Item]] = db.run(items.result)
     def getItem(id:Long):Future[Option[Item]] = db.run(items.filter(_.id === id).result.headOption)
@@ -16,6 +19,19 @@ class ItemService(val databaseService:DatabaseService)(implicit val executionCon
         case Some(item) => {
             val updateItem = Item(item.id, toUpdate.sku, toUpdate.name, toUpdate.qty)
             db.run(items.filter(_.id === id).update(updateItem)).map(_ => Some(updateItem))
+        }
+        case None => Future.successful(None)
+    }
+
+    def getItemsIn():Future[Seq[ItemIn]] = db.run(itemsIn.result)
+    def getItemIn(id:Long):Future[Option[ItemIn]] = db.run(itemsIn.filter(_.id === id).result.headOption)
+    def createItemIn(itemIn:ItemIn):Future[ItemIn] = db.run(itemsIn returning itemsIn.map(_.id) into 
+        ((itemIn, id) => itemIn.copy(id=Some(id) )) += itemIn)
+    def updateItemIn(id:Long, toUpdate:ItemIn):Future[Option[ItemIn]] = getItemIn(id).flatMap{
+        case Some(itemIn) => {
+            val updateItemIn = ItemIn(itemIn.id, toUpdate.time, toUpdate.sku, toUpdate.name, toUpdate.qty_pesan, 
+                    toUpdate.qyt_terima, toUpdate.harga_beli, toUpdate.total, toUpdate.kwitansi, toUpdate.notes)
+            db.run(itemsIn.filter(_.id === id).update(updateItemIn)).map(_ => Some(updateItemIn))
         }
         case None => Future.successful(None)
     }
