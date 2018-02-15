067c6466428c40f2655f16afdf84d3bf874b4790
aditya hadiwijaya <aditya.hadiwijaya@mncgroup.com>
2018-02-15 11:21:21 +0700

    put ItemIn table inside DAO class

--- src/main/scala/com/adit/models/db/ItemTable.scala
+++ src/main/scala/com/adit/models/db/ItemTable.scala
@@ -1,7 +1,10 @@
 package models.db
 
 import models.Item
+import models.ItemIn
+import java.sql.Timestamp
 import slick.jdbc.JdbcProfile
+
 
 case class DAO(val driver:JdbcProfile){
     import driver.api._
@@ -13,5 +16,21 @@ case class DAO(val driver:JdbcProfile){
         def qty = column[Int]("item_qty")
 
         override def * = (id.?, sku, name, qty) <> ((Item.apply _).tupled, Item.unapply)
+    }
+
+    class ItemsIn(tag:Tag) extends Table[ItemIn](tag, "ITEMS"){
+        def id = column[Long]("item_in_id", O.PrimaryKey, O.AutoInc)
+        def time = column[Timestamp]("item_in_time")
+        def sku = column[String]("item_in_sku")
+        def name = column[String]("item_in_name")
+        def qty_pesan = column[Int]("item_in_qty_pesan")
+        def qty_terima = column[Int]("item_in_qty_terima")
+        def harga_beli = column[Int]("item_in_harga_beli")
+        def total = column[Int]("item_in_total")
+        def kwitansi = column[String]("item_in_kwitansi")
+        def notes = column[String]("item_in_notes")
+
+        override def * = (id.?, time, sku, name, qty_pesan, qty_terima, harga_beli,
+                            total, kwitansi, notes) <> ((ItemIn.apply _).tupled, ItemIn.unapply)
     }
 }
